5d0142c5b961a49536cd2ce117b2df5469ebb00f
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-05-01 21:21:48 +0200

    added logistic regression

--- mycode/logistic_regression.py
+++ mycode/logistic_regression.py
@@ -22,22 +22,13 @@ def logreg_train(data, steps=10000, lr=0.01, batch_size=2000):
     m, n = X.shape
     w = np.zeros(n)
     b = 0
-    accs = []
-    losses = []
     for step in range(steps):
         index = np.random.choice(data.shape[0], batch_size, replace=False)
         newX = X[index]
         newY = Y[index]
         P = logreg_inference(newX, w, b)
-        if step % 10000 == 0:
-            loss = cross_entropy(P, newY)
-            prediction = (P > 0.5)
-            accuracy = (prediction == newY).mean()
-            accs.append(accuracy)
-            losses.append(loss)
-            print(step)
         grad_w = ((P - newY) @ newX) / batch_size
         grad_b = (P - newY).mean()
         w -= lr * grad_w
         b -= lr * grad_b
-    return w, b, accs, losses
+    return w, b
